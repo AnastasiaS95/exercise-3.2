dacad3f58ce9d304aa458a93d4756f86dcc9c6ed
AnastasiaS95 <ms.anastasia1artyukh@gmail.com>
2024-09-28 00:40:15 +0300

    Initial commit

---
+++ launch.php
@@ -0,0 +1,27 @@
+<?php
+// Объявление
+$name = 'Anastasiia';
+$age = 29;
+
+// Константа
+// Вывод значений
+echo "Имя: " .$name
+echo "Возраст: " .$age
+// Преобразование типов
+$numberString = "523"
+$number = 458
+echo "Преобразованное число: " .
+echo "Тип переменной: " .
+// Арифметические операции
+$num1 = 10;
+$num2 = 5;
+$sum = $num1 + $num2;
+$difference = $num1 - $num2;
+echo "Сложение: $sum" .
+echo "Сравнение: $difference" .
+// Логическое выражение
+echo "Логическое выражение: " .
+// Суперглобальная переменная $_SERVER
+echo "Информация о сервере: . $_SERVER['SERVER_INFORMATION']" .
+echo "IP-адрес клиента: . $_server['REMOTE_ADDR']" .
+?>
